757bf51fee24b29f3e3dcbd5982c789a46d61190
Ali Hussein <alihessein551@gmail.com>
2025-05-10 17:39:27 +0300

    رسالة التعديل

--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -1,21 +1,15 @@
 import React, { useState } from 'react'
 import Header from '../components/header'
-import Footer from '../components/Footer'
+// import Footer from '../components/Footer'
 
 function Events() {
-  const [value, setValue] = useState("vcvfdvalue");
-    function handleClick() {
-      console.log("Button clicked!");
-      setValue("ali");
-    }
-    console.log(handleClick); // تحقق مما إذا كانت الدالة معرّفة
+ 
 
   return (
     <div>
       <Header/>
-      <h1>{value}</h1>
-      <button  style={{ cursor: "pointer" }}  onClick={handleClick}>Click</button>
-   {/* <Footer/> */}
+      
+   
     </div>
   )
 }
